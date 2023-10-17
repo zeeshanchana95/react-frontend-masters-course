3c0007e4d919ce6902690b6153ba54a70e5dc0d2
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-10-17 23:41:43 +0500

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { createRoot } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cocktiel" />
-      <Pet name="Doink" animal="Cat" breed="Mix" />
+      <Pet name="Doink" animal="Cat" breed="Mix" /> */}
+      <SearchParams />;
     </div>
   );
 };
---
+++ src/SearchParams.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  //   const location = "Seattle, WA";
+  const [location, updateLocation] = useState("");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          {/* <input id="location" value={location} placeholder="Location" /> */}
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => updateLocation(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
