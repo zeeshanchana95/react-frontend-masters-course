98c41bb0c42d07b065309a058bf22afba5b6b7b9
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-10-18 12:10:48 +0500

    use of effects and api call

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   //   const location = "Seattle, WA";
+  const [pets, setPets] = useState([]);
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -63,6 +78,15 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {/* // in jsx, under form, inside the larger div */}
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
