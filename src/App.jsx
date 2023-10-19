517451b438e47726432992b824b65eebeab40842
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-10-19 09:29:15 +0500

    use of context for global state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Details from "./Details";
 import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -13,18 +16,23 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
-    <QueryClientProvider client={queryClient}>
+    <div>
       <BrowserRouter>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
       </BrowserRouter>
-    </QueryClientProvider>
+    </div>
   );
 };
 
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,15 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useContext, useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+import Modal from "./Modal";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
-import ErrorBoundary from "./ErrorBoundary";
-import { useState } from "react";
-import Modal from "./Modal";
 
 const Details = () => {
   const { id } = useParams();
-  const results = useQuery(["details", id], fetchPet);
   const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  const results = useQuery(["details", id], fetchPet);
+  // eslint-disable-next-line no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
 
   if (results.isLoading) {
     return (
@@ -23,25 +27,30 @@ const Details = () => {
 
   return (
     <div className="details">
-      <Carousel images={pet.images} />;
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>;
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
-        {
-          showModal ? (
-            <Modal>
-              <div>
-                <h1>Would you like to adopt {pet.name}?</h1>
-                <div className="buttons">
-                  <button>Yes</button>
-                  <button onClick={() => setShowModal(false)}>No</button>
-                </div>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button
+                  onClick={() => {
+                    setAdoptedPet(pet);
+                    navigate("/");
+                  }}
+                >
+                  Yes
+                </button>
+                <button onClick={() => setShowModal(false)}>No</button>
               </div>
-            </Modal>
-          ) : null // you have to remove this semi-colon, my auto-formatter adds it back if I delete it
-        }
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
