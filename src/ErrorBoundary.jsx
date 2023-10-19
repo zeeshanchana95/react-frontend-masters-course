8867f19686423ce03a1b0899cc16f7e02eda6682
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-10-19 08:55:02 +0500

    create and use Modals using Portals

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 
 class ErrorBoundary extends Component {
   state = { hasError: false };
+
   static getDerivedStateFromError() {
     return { hasError: true };
   }
+
   componentDidCatch(error, info) {
     console.error("ErrorBoundary caught an error", error, info);
   }
+
   render() {
     if (this.state.hasError) {
       return (
