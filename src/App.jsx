51c07c18ca12b93a83a1828e2ebe3195543a0a03
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-10-22 08:28:04 +0500

    using code splitting to optimize performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { useState, Suspense, lazy} from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,6 +12,9 @@ const queryClient = new QueryClient({
     },
   },
 });
+
+const Details = lazy(() => import("./Details"))
+const SearchParams = lazy(() => import("./SearchParams"))
 
 const App = () => {
   const adoptedPet = useState(null);
@@ -25,13 +26,19 @@ const App = () => {
       <BrowserRouter>
         <AdoptedPetContext.Provider value={adoptedPet}>
           <QueryClientProvider client={queryClient}>
+           <Suspense fallback={
+            <div className="loading-pane">
+                <h2 className="loader">🌀</h2>
+            </div>
+           }>
             <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500">
-              <Link className="text-6xl text-white hover:text-gray-200" to="/">Adopt Me!</Link>
-            </header>
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
+                <Link className="text-6xl text-white hover:text-gray-200" to="/">Adopt Me!</Link>
+              </header>
+              <Routes>
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/" element={<SearchParams />} />
+              </Routes>
+           </Suspense>
           </QueryClientProvider>
         </AdoptedPetContext.Provider>
       </BrowserRouter>
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -6,15 +6,15 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
     hero = images[0];
   }
   return (
-    <Link to={`details/${id}`} className="pet">
-      <div className="image-container">
-        <img src={hero} alt={name} />
-      </div>
-      <div className="info">
-        <h1>{name}</h1>
-        <h1>{`${animal} - ${breed} - ${location}`}</h1>
-      </div>
-    </Link>
+    <Link to={`/details/${id}`} className="relative block">
+  <div>
+    <img src={hero} alt={name} />
+  </div>
+  <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2">
+    <h1>{name}</h1>
+    <h2>{`${animal} — ${breed} — ${location}`}</h2>
+  </div>
+</Link>
   );
 };
 
